bc1160b39750417db5b812e0a0901431c7e39cf3
divamaisyaraasofyan-wq <divamaisyaraasofyan@gmail.com>
2026-02-09 14:12:06 +0700

    main

--- hamburger.js
+++ hamburger.js
@@ -1,9 +1,5 @@
 function toggleMenu() {
-  const nav = document.querySelector(".nav.nav-button");
-  const hamburger = document.querySelector(".hamburger");
-  const header = document.querySelector(".header");
-
-  nav.classList.toggle("show");
-  hamburger.classList.toggle("active");
-  header.classList.toggle("menu-open");
+  document.querySelector(".nav.nav-button").classList.toggle("show");
+  document.querySelector(".hamburger").classList.toggle("active");
+  document.querySelector(".header").classList.toggle("menu-open");
 }
