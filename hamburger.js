936dcb1dbe39668510b56b9308d66a314db35ed6
divamaisyaraasofyan-wq <divamaisyaraasofyan@gmail.com>
2026-02-09 15:31:30 +0700

    main

--- hamburger.js
+++ hamburger.js
@@ -3,3 +3,17 @@ function toggleMenu() {
   document.querySelector(".hamburger").classList.toggle("active");
   document.querySelector(".header").classList.toggle("menu-open");
 }
+
+document.querySelectorAll('.ig-btn').forEach(btn => {
+  btn.addEventListener('click', function (e) {
+    e.preventDefault();
+
+    const parent = this.parentElement;
+    parent.classList.toggle('active');
+
+    // tutup dropdown lain
+    document.querySelectorAll('.ig-dropdown').forEach(dd => {
+      if (dd !== parent) dd.classList.remove('active');
+    });
+  });
+});
